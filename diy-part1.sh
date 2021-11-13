1f3f206d925d03e9e0e05b29b8d8270efa7b2f52
buddyfly <buddyfly@qq.com>
2021-11-13 16:35:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git aliyundrive https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/lean/luci-app-jd-dailybonus
